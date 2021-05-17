a6ff7e83c1d4cdd82b93b5fb27d47a4b0d6d51bc
kamildlugajczyk <kam.dlugajczyk@gmail.com>
2021-05-17 19:54:44 +0200

    Create an empty spring boot project

---
+++ fleet-management/src/test/java/pl/polsl/tab/fleetmanagement/FleetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.tab.fleetmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
