b0b7015497f829c6d3c1ef889fc03ccf64e37d08
Konrad Skrzypczyk <skrzypczykmail@gmail.com>
2021-06-01 13:39:21 +0200

    Authorities information

--- src/main/java/pl/polsl/tab/fleetmanagement/auth/AuthenticationController.java
+++ src/main/java/pl/polsl/tab/fleetmanagement/auth/AuthenticationController.java
@@ -1,14 +1,20 @@
 package pl.polsl.tab.fleetmanagement.auth;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 public class AuthenticationController {
@@ -43,6 +49,12 @@ public class AuthenticationController {
         final String jwt = jwtToken.generateToken(userDetails);
 
         return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+    }
+
+    @RequestMapping(value = "/authorities", method = RequestMethod.GET)
+    @ApiOperation(value = "", authorizations = { @Authorization(value="jwtToken") })
+    public Collection<SimpleGrantedAuthority> authorities() {
+        return (Collection<SimpleGrantedAuthority>) SecurityContextHolder.getContext().getAuthentication().getAuthorities();
     }
 
 }
